f24927ffcc3fdecb28ccc825456f2cb53f4810d5
saint-sidhu <rreed8249@gmail.com>
2023-10-16 18:37:01 +0530

    Commit 3: Added a new java class which tells when a channel in a server is created/deleted.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -8,7 +8,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 public class Bot {
     public static void main(String args[]) throws Exception{
 
-        JDA jda = JDABuilder.createDefault("MTE1OTQ0ODk2ODMyNjAyMTEyMA.GYBoTP.Dww-M24Xrnpz1p9viTtiQ_cOaQc56uZ5S8-LUA")
+        JDA jda = JDABuilder.createDefault("YOUR_BOT_TOKEN_HERE")
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 //.setMemberCachePolicy(MemberCachePolicy.ALL).
                 .build();
