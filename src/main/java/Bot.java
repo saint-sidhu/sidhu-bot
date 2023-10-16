c2dd7bf93bfdee0459721d5cfddb82b1e7ac7be5
saint-sidhu <rreed8249@gmail.com>
2023-10-16 18:35:57 +0530

    Commit 3: Added a new java class which tells when a channel in a server is created/deleted.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,3 +1,4 @@
+import dev.overlord.events.ChannelEvent;
 import dev.overlord.events.HelloEvent;
 import dev.overlord.events.PingPongEvent;
 import net.dv8tion.jda.api.JDA;
@@ -7,7 +8,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 public class Bot {
     public static void main(String args[]) throws Exception{
 
-        JDA jda = JDABuilder.createDefault("YOUR_BOT_TOKEN_HERE")
+        JDA jda = JDABuilder.createDefault("MTE1OTQ0ODk2ODMyNjAyMTEyMA.GYBoTP.Dww-M24Xrnpz1p9viTtiQ_cOaQc56uZ5S8-LUA")
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 //.setMemberCachePolicy(MemberCachePolicy.ALL).
                 .build();
@@ -15,6 +16,7 @@ public class Bot {
 
         jda.addEventListener(new HelloEvent());
         jda.addEventListener(new PingPongEvent());
+        jda.addEventListener(new ChannelEvent());
     }
 
 }
---
+++ src/main/java/dev/overlord/events/ChannelEvent.java
@@ -0,0 +1,28 @@
+package dev.overlord.events;
+
+import net.dv8tion.jda.api.events.channel.ChannelCreateEvent;
+import net.dv8tion.jda.api.events.channel.ChannelDeleteEvent;
+import net.dv8tion.jda.api.events.channel.update.GenericChannelUpdateEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class ChannelEvent extends ListenerAdapter {
+    public void onChannelCreate(ChannelCreateEvent event){
+
+        //when we create a new channel/category it sends this message in #general
+        event.getGuild().getDefaultChannel().asTextChannel().sendMessage
+                ("Someone just created a Category/Channel in " + event.getGuild().getName()).queue();
+
+        //event.getGuild().getDefaultChannel().asTextChannel().sendMessage
+                //("Someone just created a Voice Channel in " + event.getGuild().getName()).queue();
+
+    }
+
+    public void onChannelDelete(ChannelDeleteEvent event){
+        event.getGuild().getDefaultChannel().asTextChannel().sendMessage
+                ("Someone just deleted a Category/Channel in " + event.getGuild().getName()).queue();
+    }
+
+    public void onGenericChannelUpdate(GenericChannelUpdateEvent event){
+
+    }
+}
